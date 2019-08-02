3a6b5ed6f4f33ac13335c1c06f188f0b160d05e8
Kristen Payne <kristen@polysign.io>
2019-08-01 22:54:47 -0700

    Implement test digester

--- quiz.py
+++ quiz.py
@@ -1,35 +1,42 @@
 
 def digest_quiz(file):
     quizzes = {}
+    current_question = ''
+    current_quiz = 'quiz1'
+    answer_tokens = '+-'
+    quiz_counter = 1
+    question_counter = 1
+    quizzes[current_quiz] = list()
     for line in open(file):
-        current_quiz = ''
-        current_question = ''
-        answer_tokens = '+-'
-        quiz_counter = 1
-        question_counter = 1
+        line = line.rstrip()
+        if line == "END":
+            quizzes[current_quiz].append(current_question)
+            break
         if line[0] == '=':
             current_quiz = line[1:]
-        else:
-            current_quiz = f'quiz_{quiz_counter}'
-            counter += 1        
-        quizzes[current_quiz] = list()
+            quizzes[current_quiz] = list()
         if not line:
             continue
         if line[0] not in answer_tokens:
+            if current_question:
+                if len(current_question['answer']) < 1:
+                    current_question['choice'] = True
+                quizzes[current_quiz].append(current_question)
             current_question = {
                 'question': line,
                 'choice': False,
+                'answer': '',
             }
-            quizzes[current_quiz].append({
-                multiple_choice: False,
-                answers: list()
-            )
             question_counter += 1
             continue
         else:
             if line[0] == '-':
-                quizzes[current_quiz][]
+                current_question['answer'] = (line[1:], False)
+            if line[0] == '+':
+                current_question['answer'] = (line[1:], True)
+    return quizzes
 
 
 
-digest_quiz("./sample.txt")
+print(digest_quiz("./sample.txt"))
+
