00ee2a085c04acb7d09d4c967f57dce89892bcf3
Kristen Payne <victorycabbage@gmail.com>
2019-08-02 09:21:36 -0700

    Question handling

--- quiz.py
+++ quiz.py
@@ -1,3 +1,6 @@
+import shelve
+
+master_quiz = shelve.open('quiz')
 
 def digest_quiz(file):
     quizzes = {}
@@ -25,18 +28,57 @@ def digest_quiz(file):
             current_question = {
                 'question': line,
                 'choice': False,
-                'answer': '',
+                'answer': list(),
             }
             question_counter += 1
             continue
         else:
             if line[0] == '-':
-                current_question['answer'] = (line[1:], False)
+                current_question['answer'].append((line[1:], False))
             if line[0] == '+':
-                current_question['answer'] = (line[1:], True)
+                current_question['answer'].append((line[1:], True))
     return quizzes
 
+master_quiz = digest_quiz("./sample.txt")
 
+def run_quiz():
+    name_dict = {}
+    for index,quiz in enumerate(master_quiz.keys(),1):
+        name_dict[index] = quiz
+        print(f'{index}. {quiz}')
+    selected_quiz = input('Which quiz do you want to run?')
+    selected_quiz = name_dict.get(selected_quiz,None)
+    if not selected_quiz:
+        print("Something went wrong. Try again.")
+    return selected_quiz
 
-print(digest_quiz("./sample.txt"))
+def ask_questions(quiz):
+    for question in master_quiz[quiz]:
+        if question['choices']:
+            handle_multiple_choice(question)
+        else:
+            handle_flashcard(question)
+
+def handle_multiple_choice(q):
+    answer_dict = {}
+    print(q['question'])
+    #TODO: randomize the answer output
+    for answer,index in enumerate(q['answer'],1):
+        answer_dict[index] = answer
+        print(answer[0])
+    your_answer = 0
+    while not your_answer:
+        your_answer = int(input('What is your choice?'))
+        if your_answer >= len(q['answer']) or your_answer < 0:
+            print("That is not a valid choice")
+            your_answer = 0
+
+def handle_flashcard(q):
+    print(q['question'])
+    print('Press return to reveal answer.')
+    your_input = input('Ready?>>')
+    print(q['answer'])
+    return
+
+print(master_quiz)
 
